f9810d8e0e39cde11235174fa283489e969771a1
xkerria <xkerria@126.com>
2021-08-26 10:57:39 +0800

    调整router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import store from '@/store'
 import users from './users'
 import categories from './categories'
 import services from './services'
+import settings from './settings'
 
 export const routes = [
   {
@@ -24,62 +25,7 @@ export const routes = [
       categories,
       services,
       users,
-      {
-        path: 'settings',
-        meta: {
-          label: '系统设置',
-          icon: 'md-tune-r'
-        },
-        component: () => import('../views/setting/index.vue'),
-        children: [
-          {
-            path: '',
-            name: 'setting-list',
-            meta: {
-              label: '参数设置',
-              icon: 'md-setting-fr'
-            },
-            component: () => import('../views/setting/List.vue')
-          },
-          {
-            path: 'admins',
-            name: 'admin-list',
-            meta: {
-              label: '管理员',
-              icon: 'md-admin-fr'
-            },
-            component: () => import('../views/admin/List.vue')
-          },
-          {
-            path: 'admins/add',
-            name: 'admin-add',
-            meta: {
-              label: '添加管理员',
-              hidden: true
-            },
-            component: () => import('../views/admin/Add.vue')
-          },
-          {
-            path: 'admins/:id/edit',
-            name: 'admin-edit',
-            props: true,
-            meta: {
-              label: '编辑管理员',
-              hidden: true
-            },
-            component: () => import('../views/admin/Edit.vue')
-          },
-          {
-            path: 'logs',
-            name: 'log-list',
-            meta: {
-              label: '日志查询',
-              icon: 'md-summarize-fr'
-            },
-            component: () => import('../views/log/List.vue')
-          }
-        ]
-      }
+      settings
     ]
   },
   {
--- src/router/settings.js
+++ src/router/settings.js
@@ -1,57 +1,56 @@
 export default {
   path: 'settings',
-  name: 'settings',
-  component: () => import('../views/setting/index.vue'),
   meta: {
-    title: '系统设置',
+    label: '系统设置',
     icon: 'md-tune-r'
   },
+  component: () => import('@/views/setting/index.vue'),
   children: [
     {
       path: '',
       name: 'setting-list',
       meta: {
-        title: '基本配置',
+        label: '参数设置',
         icon: 'md-setting-fr'
       },
-      component: () => import('../views/setting/List.vue')
+      component: () => import('@/views/setting/List.vue')
     },
     {
       path: 'admins',
       name: 'admin-list',
       meta: {
-        title: '管理员',
+        label: '管理员',
         icon: 'md-admin-fr'
       },
-      component: () => import('../views/admin/List.vue')
+      component: () => import('@/views/admin/List.vue')
     },
     {
       path: 'admins/add',
       name: 'admin-add',
       meta: {
-        title: '添加管理员',
+        label: '添加管理员',
         hidden: true
       },
-      component: () => import('../views/admin/Add.vue')
+      component: () => import('@/views/admin/Add.vue')
     },
     {
       path: 'admins/:id/edit',
       name: 'admin-edit',
       props: true,
       meta: {
-        title: '编辑管理员',
+        label: '编辑管理员',
         hidden: true
       },
-      component: () => import('../views/admin/Edit.vue')
+      component: () => import('@/views/admin/Edit.vue')
     },
     {
       path: 'logs',
       name: 'log-list',
       meta: {
-        title: '日志查询',
+        label: '日志查询',
         icon: 'md-summarize-fr'
       },
-      component: () => import('../views/log/List.vue')
+      component: () => import('@/views/log/List.vue')
     }
   ]
 }
